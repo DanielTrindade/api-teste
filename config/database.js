a0fe421b3c752cef0d8952f7e4cf75346a8cc966
Daniel Trindade <danieloliveiratrindade@gmail.com>
2023-02-10 19:10:50 -0400

    criando a tabela e model de usuários.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
   username: 'root',
-  password: '',
-  database: 'node teste',
-  host: '187.14.17.67',
+  password: 'root',
+  database: 'expensesDB',
+  host: 'localhost',
   dialect: 'mariadb',
 }
